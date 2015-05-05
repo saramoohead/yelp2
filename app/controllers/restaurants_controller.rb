dcb67f0459cea83e1a5f75a9b133506fcd6c0949
James Miller <james.fl.miller@live.co.uk>
2015-05-05 12:40:17 +0100

    Add restaurant profile pages

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,12 +3,16 @@ class RestaurantsController < ApplicationController
     @restaurants = Restaurant.all
   end
   def new
+    @restaurant = Restaurant.new
   end
   def create
-    Restaurant.create(restaurant_params)
+    @restaurant = Restaurant.create(restaurant_params)
     redirect_to '/restaurants'
   end
   def restaurant_params
     params.require(:restaurant).permit(:name)
+  end
+  def show
+    @restaurant = Restaurant.find(params[:id])
   end
 end
